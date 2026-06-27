d5ec742021b48951cc62031338c185d761be2042
cfontes <caiquefontes@outlook.com>
2026-06-26 21:57:39 -0300

    add system authenticated native django for post visible

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AUTH
+LOGIN_REDIRECT_URL = '/'
